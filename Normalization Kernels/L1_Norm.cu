8783c6e8e82d7c223792035495b5c61c7f476cce
Abhay <abhaykarpe45@gmail.com>
2025-05-04 23:48:42 +0200

    L1 Norm

---
+++ Normalization Kernels/L1_Norm.cu
@@ -0,0 +1,42 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+#define BLOCK_DIM 1024
+
+__global__ void L1_Norm_kernel(const float* X, float* Y, size_t B, size_t D) {
+
+    extern __shared__ float partialSum_s[];
+
+    int b = blockIdx.x;      
+    int tid = threadIdx.x;
+
+    float sum = 0.0f;
+
+    for (int d = tid; d < D; d += blockDim.x) {
+        sum += fabsf(X[b * D + d]);
+    }
+
+    partialSum_s[tid] = sum + 1e-8f;
+    __syncthreads();
+
+    #pragma unroll 10
+    for (int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
+        if (tid < stride) {
+            partialSum_s[tid] += partialSum_s[tid + stride];
+        }
+        __syncthreads();
+    }
+
+    float summ = partialSum_s[0];
+    __syncthreads();
+
+    for (int d = tid; d < D; d += blockDim.x) {
+        Y[b * D + d] = X[b * D + d] / (summ );
+    }
+}
+
+
+
+extern "C" void solution(const float* X, float* Y, size_t B, size_t D) {
+    L1_Norm_kernel<<<B, BLOCK_DIM, BLOCK_DIM * sizeof(float)>>>(X, Y, B, D);
+}
