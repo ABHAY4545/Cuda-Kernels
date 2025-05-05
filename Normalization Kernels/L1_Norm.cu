f13b522eb08e1e0d6c02912a160817acbe4ea606
Abhay <abhaykarpe45@gmail.com>
2025-05-05 22:31:47 +0200

    L1 Kernel optimized for perf from 260 GFLOPS to 300 GFLOPS

--- Normalization Kernels/L1_Norm.cu
+++ Normalization Kernels/L1_Norm.cu
@@ -3,23 +3,23 @@
 
 #define BLOCK_DIM 1024
 
-__global__ void L1_Norm_kernel(const float* X, float* Y, size_t B, size_t D) {
+__global__ void L1_Norm_kernel(const float* __restrict__ X, float* __restrict__ Y, size_t B, size_t D) {
 
     extern __shared__ float partialSum_s[];
 
     int b = blockIdx.x;      
     int tid = threadIdx.x;
+    int offset = b * D;
 
     float sum = 0.0f;
 
     for (int d = tid; d < D; d += blockDim.x) {
-        sum += fabsf(X[b * D + d]);
+        sum += fabsf(X[offset + d]);
     }
 
-    partialSum_s[tid] = sum + 1e-8f;
+    partialSum_s[tid] = sum ;
     __syncthreads();
 
-    #pragma unroll 10
     for (int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
         if (tid < stride) {
             partialSum_s[tid] += partialSum_s[tid + stride];
@@ -27,16 +27,16 @@ __global__ void L1_Norm_kernel(const float* X, float* Y, size_t B, size_t D) {
         __syncthreads();
     }
 
-    float summ = partialSum_s[0];
-    __syncthreads();
+    float summ = partialSum_s[0] + 1e-8f;
 
     for (int d = tid; d < D; d += blockDim.x) {
-        Y[b * D + d] = X[b * D + d] / (summ );
+        Y[offset + d] = X[offset + d] / (summ );
     }
 }
 
 
 
 extern "C" void solution(const float* X, float* Y, size_t B, size_t D) {
+
     L1_Norm_kernel<<<B, BLOCK_DIM, BLOCK_DIM * sizeof(float)>>>(X, Y, B, D);
 }
