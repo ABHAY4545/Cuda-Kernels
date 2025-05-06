5b141dd2553be9894e7b06492abf5b38bb98a950
Abhay <abhaykarpe45@gmail.com>
2025-05-06 23:53:00 +0200

    Improved perf from 330FLOPS  to 394 GFOPS

--- Normalization Kernels/L1_Norm.cu
+++ Normalization Kernels/L1_Norm.cu
@@ -1,42 +1,98 @@
+//L1 Norm Vectorized
 #include <cuda_runtime.h>
 #include <math.h>
 
 #define BLOCK_DIM 1024
 
-__global__ void L1_Norm_kernel(const float* __restrict__ X, float* __restrict__ Y, size_t B, size_t D) {
+__global__ void L1_Norm_kernel(const float* __restrict__ X, float* __restrict__ Y, size_t B, size_t D){
+    int row = blockIdx.x;
+    if (row >= B) return;
 
-    extern __shared__ float partialSum_s[];
+    __shared__ float warp_sums[32];
+    __shared__ float l1_norm;
 
-    int b = blockIdx.x;      
-    int tid = threadIdx.x;
-    int offset = b * D;
+    const float* ptr = X + row * D;
+    const float4* ptr_4 = reinterpret_cast<const float4*>(ptr);
 
     float sum = 0.0f;
+    int n_float4 = D / 4;
 
-    for (int d = tid; d < D; d += blockDim.x) {
-        sum += fabsf(X[offset + d]);
+    for(int i = threadIdx.x; i < n_float4; i += BLOCK_DIM){
+        float4 v = ptr_4[i];
+        sum += fabsf(v.x) + fabsf(v.y) + fabsf(v.z) + fabsf(v.w);
     }
 
-    partialSum_s[tid] = sum ;
+    for(int offset = 16; offset > 0; offset >>= 1){
+        sum += __shfl_down_sync(0xffffffff, sum, offset);
+    }
+
+    if ((threadIdx.x & 31) == 0)
+        warp_sums[threadIdx.x / 32] = sum;
+
     __syncthreads();
 
-    for (int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
-        if (tid < stride) {
-            partialSum_s[tid] += partialSum_s[tid + stride];
+    if (threadIdx.x < 32){
+        sum = (threadIdx.x < (BLOCK_DIM / 32)) ? warp_sums[threadIdx.x] : 0.0f;
+        for (int offset = 16; offset > 0; offset >>= 1){
+            sum += __shfl_down_sync(0xffffffff, sum, offset);
         }
-        __syncthreads();
+        if (threadIdx.x == 0)
+            l1_norm = sum;
     }
 
-    float summ = partialSum_s[0] + 1e-8f;
+    __syncthreads();
 
-    for (int d = tid; d < D; d += blockDim.x) {
-        Y[offset + d] = X[offset + d] / (summ );
+    float norm = l1_norm;
+    for(int d = threadIdx.x; d < D; d += BLOCK_DIM){
+        Y[row * D + d] = X[row * D + d] / (norm + 1e-8f);
     }
 }
 
 
-
-extern "C" void solution(const float* X, float* Y, size_t B, size_t D) {
-
-    L1_Norm_kernel<<<B, BLOCK_DIM, BLOCK_DIM * sizeof(float)>>>(X, Y, B, D);
+extern "C" void solution(const float* X, float* Y, size_t B, size_t D){
+    L1_Norm_kernel<<<B, BLOCK_DIM>>>(X, Y, B, D);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
