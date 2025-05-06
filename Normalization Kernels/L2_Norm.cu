9248cc4b3a8c7d0e94a17a395ccc8e390cca3122
Abhay <abhaykarpe45@gmail.com>
2025-05-07 00:31:53 +0200

    Improved perf of L2 Norm from 330 GFLOPS to 400 GFLOPS

--- Normalization Kernels/L2_Norm.cu
+++ Normalization Kernels/L2_Norm.cu
@@ -1,47 +1,4 @@
-#include <cuda_runtime.h>
-#include <math.h>
-
-#define BLOCK_DIM 1024
-
-__global__ void L2_Norm_kernel(const float* __restrict__ X, float* __restrict__ Y, size_t B, size_t D) {
-
-    extern __shared__ float partialSum_s[];
-
-    int b = blockIdx.x;      
-    int tid = threadIdx.x;
-    int offset = b * D;
-
-    float sum = 0.0f;
-
-    for (int d = tid; d < D; d += blockDim.x) {
-        sum += ( X[offset + d] * X[offset + d]);
-    }
-
-    partialSum_s[tid] = sum ;
-    __syncthreads();
-
-    for (int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
-        if (tid < stride) {
-            partialSum_s[tid] += partialSum_s[tid + stride];
-        }
-        __syncthreads();
-    }
-
-    float summ = partialSum_s[0] + 1e-8f;
-
-    for (int d = tid; d < D; d += blockDim.x) {
-        Y[offset + d] = X[offset + d] / (sqrtf(summ) );
-    }
-}
-
-//current perf 290+ GFLOPS on tensara
-
-extern "C" void solution(const float* X, float* Y, size_t B, size_t D) {
-    L2_Norm_kernel<<<B, BLOCK_DIM, BLOCK_DIM * sizeof(float)>>>(X, Y, B, D);
-}
-
-
-// Vectorized Version perf 400 GFLOPS
+//L2 Norm Vectorized
 #include <cuda_runtime.h>
 #include <math.h>
 
@@ -90,6 +47,5 @@ __global__ void L2_Norm_kernel(const float* __restrict__ X, float* __restrict__ 
 }
 
 extern "C" void solution(const float* X, float* Y, size_t B, size_t D) {
-
     L2_Norm_kernel<<<B, BLOCK_DIM, BLOCK_DIM * sizeof(float)>>>(X, Y, B, D);
 }
